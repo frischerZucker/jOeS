cdc03848634bf4d47843098236b14697741ae229
Moritz Bärnt <moritzbaernt@outlook.de>
2025-07-18 11:23:02 +0200

    Make build script use script-relative paths instead of working directoy.
    The build script now determines its own directory and uses paths relative to
    that location, ensuring it works correctly regardless of the current working
    directory.

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
+
 # Change to your toolchains location.
 export PATH="$HOME/opt/cross/bin:$PATH"
+
+# Get the absolute path to this script. 
+export SCRIPT_LOCATION=$(cd -- $(dirname -- "${BASH_SOURCE[0]}"); pwd)
+# Get the projects root directory. -> parent directory of where this script is located
+export PROJECT_ROOT_DIR="$SCRIPT_LOCATION/.."
+
+cd $PROJECT_ROOT_DIR
 
 cd kernel
 make TOOLCHAIN_PREFIX=x86_64-elf-
@@ -7,7 +16,7 @@ if [ $? != 0 ]; then
     echo "ERROR: Building the kernel failed!"
     exit
 fi
-cd ..
+cd $PROJECT_ROOT_DIR
 
 make iso
 if [ $? != 0 ]; then
