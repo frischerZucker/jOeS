fef9d4cce669dfc49b659c41bb58655485ed3120
Moritz Bärnt <moritzbaernt@outlook.de>
2025-07-18 10:55:18 +0200

    Better build sript.
    The build script now uses the correct toolchain for building the kernel.
    It also adds the toolchains location to $PATH and throws errors, if a step fails.

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
+# Change to your toolchains location.
+export PATH="$HOME/opt/cross/bin:$PATH"
+
 cd kernel
-make
+make TOOLCHAIN_PREFIX=x86_64-elf-
+if [ $? != 0 ]; then
+    echo "ERROR: Building the kernel failed!"
+    exit
+fi
 cd ..
 
 make iso
+if [ $? != 0 ]; then
+    echo "ERROR: Creating an .iso failed!"
+    exit
+fi
 
 make run
+if [ $? != 0 ]; then
+    echo "ERROR: Running the .iso using QEMU failed!"
+    exit
+fi
