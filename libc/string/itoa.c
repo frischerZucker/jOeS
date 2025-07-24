52408249225a52d657ea42862cd6b406e1c5c8b3
Moritz Bärnt <moritzbaernt@outlook.de>
2025-07-24 21:14:53 +0200

    Add comments for iota() and strrev().

--- libc/string/itoa.c
+++ libc/string/itoa.c
@@ -2,27 +2,37 @@
 
 #include <stdint.h>
 
+/*
+    Converts an integer to a string in a given base.
+
+    @param number Integer to convert.
+    @param str Char array where the result will be stored.
+    @param base Base used for the conversion.
+*/
 void itoa(int number, char *str, int base)
 {
     int temp;
     size_t i = 0;
     uint8_t sign = 0;
 
+    // The normal conversion doesn't work for 0, so we directly return the string "0\0".
     if (number == 0)
     {
         str[0] = '0';
         str[1] = '\0';
         return;
     }
-    
+    // Remember the sign so we can add a '-' to the string if needed.
     if (number < 0)
     {
         sign = 1;
     }
 
+    // Continous division by the target base. The remainder is our digit.
     while (number != 0)
     {
         temp = number % base;
+        // temp should always be positive, otherwise (temp > 9) won't work.
         if (sign)
         {
             temp = temp * -1;
@@ -32,7 +42,7 @@ void itoa(int number, char *str, int base)
         number = number / base;
         i = i + 1;
     }
-    
+    // Add a '-' if the number was negative.
     if (sign)
     {
         str[i] = '-';
@@ -41,5 +51,6 @@ void itoa(int number, char *str, int base)
     
     str[i] = '\0';
 
+    // The algorithm built the string in reverse order, so we have to reverse it.
     strrev(str);
 }
--- libc/string/strrev.c
+++ libc/string/strrev.c
@@ -1,17 +1,23 @@
 #include "string.h"
 
+/*
+    Reverses a string in place.
+
+    @param str String to reverse.
+*/
 void strrev(char *str)
 {
     size_t i = 0;
     size_t j = strlen(str) - 1;
     char temp;
 
+    // Swap a value from the front with one from the back until the indices meet in the middle.
     while (i < j)
     {
         temp = str[i];
         str[i] = str[j];
         str[j] = temp;
-
+        // Move the indices closer together.
         i = i + 1;
         j = j - 1;
     } 
