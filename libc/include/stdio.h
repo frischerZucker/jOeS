64a12a509afdd015be7220f68fafc2b0a06bd073
moritz <62518490+frischerZucker@users.noreply.github.com>
2025-08-28 21:25:23 +0200

    Move local macros from stdio.h to printf.c.

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -3,18 +3,6 @@
 
 #include <stddef.h>
 
-// Maximum characters needed to represent an int.
-#define INT_MAX_CHARS 12
-
-#define STATE_NORMAL 0
-#define STATE_FORMAT 1
-
-#define FORMAT_SPECIFIER '%'
-#define FORMAT_STRING 's'   
-#define FORMAT_CHAR 'c'
-#define FORMAT_INT1 'd'
-#define FORMAT_INT2 'i'
-
 size_t printf(char *str, ...);
 
 #endif // STDIO_H
--- libc/stdio/printf.c
+++ libc/stdio/printf.c
@@ -6,6 +6,18 @@
 #include "string.h"
 #include "terminal.h"
 
+// Maximum characters needed to represent an int.
+#define INT_MAX_CHARS 12
+
+#define STATE_NORMAL 0
+#define STATE_FORMAT 1
+
+#define FORMAT_SPECIFIER '%'
+#define FORMAT_STRING 's'   
+#define FORMAT_CHAR 'c'
+#define FORMAT_INT1 'd'
+#define FORMAT_INT2 'i'
+
 /*
     Writes a string to the terminal and substitutes format specifiers (%...) with variables.
     
