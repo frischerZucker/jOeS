1d925534b9a3856a3bd70440e5158d89da758300
Moritz Bärnt <moritzbaernt@outlook.de>
2025-09-09 23:01:41 +0200

    Make hcf() header only.

--- kernel/include/cpu/hcf.h
+++ kernel/include/cpu/hcf.h
@@ -1,6 +1,17 @@
 #ifndef HCF_H
 #define HCF_H
 
-void hcf();
+/*
+    Halt-And-Catch-Fire function.
+
+    Stops all action and does nothing.
+*/
+static inline void hcf(void)
+{
+    while (1)
+    {
+        asm("hlt");
+    }
+}
 
 #endif
