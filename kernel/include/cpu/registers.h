7e908c9d536a4ffec1099db41ccaa6e4dc920cd9
Moritz Bärnt <moritzbaernt@outlook.de>
2026-05-18 21:23:08 +0200

    Add function for reading CR3 register.

---
+++ kernel/include/cpu/registers.h
@@ -0,0 +1,18 @@
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+#include <stdint.h>
+
+static inline uint64_t read_cr3()
+{
+    uint64_t cr3;
+
+    asm(
+        "mov %%cr3, %0"
+        : "=r"(cr3)
+    );
+
+    return cr3;    
+}
+
+#endif // REGISTERS_H
