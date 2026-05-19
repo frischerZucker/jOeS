e276b27dbf6307226b6ef86593de5a34a362d721
Moritz Bärnt <moritzbaernt@outlook.de>
2026-05-19 21:23:28 +0200

    Add function for setting cr3.

--- kernel/include/cpu/registers.h
+++ kernel/include/cpu/registers.h
@@ -15,4 +15,13 @@ static inline uint64_t read_cr3()
     return cr3;    
 }
 
+static inline void set_cr3(uint64_t value)
+{
+    asm(
+        "mov %0, %%cr3"
+        :
+        :"r"(value)
+    );
+}
+
 #endif // REGISTERS_H
